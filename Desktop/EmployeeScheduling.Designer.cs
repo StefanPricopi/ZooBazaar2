5e3da6c5fe44393174bbf0e1bee6ec521a6f9c74
522201@student.fontys.nl <m.partalov@student.fontys.nl>
2023-11-15 13:31:12 +0100

    Changes

---
+++ Desktop/EmployeeScheduling.Designer.cs
@@ -0,0 +1,426 @@
+﻿namespace Desktop
+{
+    partial class EmployeeScheduling
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            panel2 = new Panel();
+            panel8 = new Panel();
+            panel1 = new Panel();
+            panel4 = new Panel();
+            panel3 = new Panel();
+            panel6 = new Panel();
+            panel5 = new Panel();
+            panel7 = new Panel();
+            panel9 = new Panel();
+            panel10 = new Panel();
+            panel11 = new Panel();
+            panel12 = new Panel();
+            panel13 = new Panel();
+            panel15 = new Panel();
+            panel14 = new Panel();
+            btnNext = new Button();
+            btnPrevious = new Button();
+            lbSunday = new Label();
+            lbMonday = new Label();
+            lbTuesday = new Label();
+            lbWednesday = new Label();
+            tbThursday = new Label();
+            lbFriday = new Label();
+            lbSaturday = new Label();
+            panel16 = new Panel();
+            panel17 = new Panel();
+            panel18 = new Panel();
+            panel19 = new Panel();
+            panel20 = new Panel();
+            panel21 = new Panel();
+            panel22 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            flowLayoutPanel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.Screenshot_12;
+            pictureBox1.Location = new Point(-8, 511);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(1669, 54);
+            pictureBox1.TabIndex = 18;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.Screenshot_12;
+            pictureBox2.Location = new Point(-8, -5);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(885, 62);
+            pictureBox2.TabIndex = 21;
+            pictureBox2.TabStop = false;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(panel2);
+            flowLayoutPanel1.Controls.Add(panel1);
+            flowLayoutPanel1.Controls.Add(panel4);
+            flowLayoutPanel1.Controls.Add(panel3);
+            flowLayoutPanel1.Controls.Add(panel6);
+            flowLayoutPanel1.Controls.Add(panel5);
+            flowLayoutPanel1.Controls.Add(panel7);
+            flowLayoutPanel1.Controls.Add(panel16);
+            flowLayoutPanel1.Controls.Add(panel9);
+            flowLayoutPanel1.Controls.Add(panel10);
+            flowLayoutPanel1.Controls.Add(panel11);
+            flowLayoutPanel1.Controls.Add(panel12);
+            flowLayoutPanel1.Controls.Add(panel13);
+            flowLayoutPanel1.Controls.Add(panel15);
+            flowLayoutPanel1.Controls.Add(panel17);
+            flowLayoutPanel1.Controls.Add(panel14);
+            flowLayoutPanel1.Controls.Add(panel18);
+            flowLayoutPanel1.Controls.Add(panel19);
+            flowLayoutPanel1.Controls.Add(panel21);
+            flowLayoutPanel1.Controls.Add(panel20);
+            flowLayoutPanel1.Controls.Add(panel22);
+            flowLayoutPanel1.Location = new Point(12, 88);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(800, 401);
+            flowLayoutPanel1.TabIndex = 22;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(panel8);
+            panel2.Location = new Point(3, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(104, 68);
+            panel2.TabIndex = 1;
+            // 
+            // panel8
+            // 
+            panel8.Location = new Point(3, 74);
+            panel8.Name = "panel8";
+            panel8.Size = new Size(104, 68);
+            panel8.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            panel1.Location = new Point(113, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(104, 68);
+            panel1.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            panel4.Location = new Point(223, 3);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(104, 68);
+            panel4.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            panel3.Location = new Point(333, 3);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(104, 68);
+            panel3.TabIndex = 0;
+            // 
+            // panel6
+            // 
+            panel6.Location = new Point(443, 3);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(104, 68);
+            panel6.TabIndex = 1;
+            // 
+            // panel5
+            // 
+            panel5.Location = new Point(553, 3);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(104, 68);
+            panel5.TabIndex = 0;
+            // 
+            // panel7
+            // 
+            panel7.Location = new Point(663, 3);
+            panel7.Name = "panel7";
+            panel7.Size = new Size(104, 68);
+            panel7.TabIndex = 0;
+            // 
+            // panel9
+            // 
+            panel9.Location = new Point(113, 77);
+            panel9.Name = "panel9";
+            panel9.Size = new Size(104, 68);
+            panel9.TabIndex = 0;
+            // 
+            // panel10
+            // 
+            panel10.Location = new Point(223, 77);
+            panel10.Name = "panel10";
+            panel10.Size = new Size(104, 68);
+            panel10.TabIndex = 1;
+            // 
+            // panel11
+            // 
+            panel11.Location = new Point(333, 77);
+            panel11.Name = "panel11";
+            panel11.Size = new Size(104, 68);
+            panel11.TabIndex = 1;
+            // 
+            // panel12
+            // 
+            panel12.Location = new Point(443, 77);
+            panel12.Name = "panel12";
+            panel12.Size = new Size(104, 68);
+            panel12.TabIndex = 1;
+            // 
+            // panel13
+            // 
+            panel13.Location = new Point(553, 77);
+            panel13.Name = "panel13";
+            panel13.Size = new Size(104, 68);
+            panel13.TabIndex = 1;
+            // 
+            // panel15
+            // 
+            panel15.Location = new Point(663, 77);
+            panel15.Name = "panel15";
+            panel15.Size = new Size(104, 68);
+            panel15.TabIndex = 1;
+            // 
+            // panel14
+            // 
+            panel14.Location = new Point(113, 151);
+            panel14.Name = "panel14";
+            panel14.Size = new Size(104, 68);
+            panel14.TabIndex = 1;
+            // 
+            // btnNext
+            // 
+            btnNext.Location = new Point(725, 521);
+            btnNext.Name = "btnNext";
+            btnNext.Size = new Size(75, 23);
+            btnNext.TabIndex = 23;
+            btnNext.Text = "Next";
+            btnNext.UseVisualStyleBackColor = true;
+            // 
+            // btnPrevious
+            // 
+            btnPrevious.Location = new Point(644, 521);
+            btnPrevious.Name = "btnPrevious";
+            btnPrevious.Size = new Size(75, 23);
+            btnPrevious.TabIndex = 24;
+            btnPrevious.Text = "Previous";
+            btnPrevious.UseVisualStyleBackColor = true;
+            // 
+            // lbSunday
+            // 
+            lbSunday.AutoSize = true;
+            lbSunday.Font = new Font("Century Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lbSunday.Location = new Point(18, 60);
+            lbSunday.Name = "lbSunday";
+            lbSunday.Size = new Size(80, 22);
+            lbSunday.TabIndex = 25;
+            lbSunday.Text = "Sunday";
+            // 
+            // lbMonday
+            // 
+            lbMonday.AutoSize = true;
+            lbMonday.Font = new Font("Century Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lbMonday.Location = new Point(127, 60);
+            lbMonday.Name = "lbMonday";
+            lbMonday.Size = new Size(88, 22);
+            lbMonday.TabIndex = 26;
+            lbMonday.Text = "Monday";
+            // 
+            // lbTuesday
+            // 
+            lbTuesday.AutoSize = true;
+            lbTuesday.Font = new Font("Century Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lbTuesday.Location = new Point(236, 60);
+            lbTuesday.Name = "lbTuesday";
+            lbTuesday.Size = new Size(86, 22);
+            lbTuesday.TabIndex = 27;
+            lbTuesday.Text = "Tuesday";
+            // 
+            // lbWednesday
+            // 
+            lbWednesday.AutoSize = true;
+            lbWednesday.Font = new Font("Century Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lbWednesday.Location = new Point(343, 60);
+            lbWednesday.Name = "lbWednesday";
+            lbWednesday.Size = new Size(120, 22);
+            lbWednesday.TabIndex = 28;
+            lbWednesday.Text = "Wednesday";
+            // 
+            // tbThursday
+            // 
+            tbThursday.AutoSize = true;
+            tbThursday.Font = new Font("Century Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            tbThursday.Location = new Point(469, 60);
+            tbThursday.Name = "tbThursday";
+            tbThursday.Size = new Size(92, 22);
+            tbThursday.TabIndex = 29;
+            tbThursday.Text = "Thursday";
+            // 
+            // lbFriday
+            // 
+            lbFriday.AutoSize = true;
+            lbFriday.Font = new Font("Century Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lbFriday.Location = new Point(586, 60);
+            lbFriday.Name = "lbFriday";
+            lbFriday.Size = new Size(65, 22);
+            lbFriday.TabIndex = 30;
+            lbFriday.Text = "Friday";
+            // 
+            // lbSaturday
+            // 
+            lbSaturday.AutoSize = true;
+            lbSaturday.Font = new Font("Century Gothic", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lbSaturday.Location = new Point(675, 60);
+            lbSaturday.Name = "lbSaturday";
+            lbSaturday.Size = new Size(94, 22);
+            lbSaturday.TabIndex = 31;
+            lbSaturday.Text = "Saturday";
+            // 
+            // panel16
+            // 
+            panel16.Location = new Point(3, 77);
+            panel16.Name = "panel16";
+            panel16.Size = new Size(104, 68);
+            panel16.TabIndex = 1;
+            // 
+            // panel17
+            // 
+            panel17.Location = new Point(3, 151);
+            panel17.Name = "panel17";
+            panel17.Size = new Size(104, 68);
+            panel17.TabIndex = 1;
+            // 
+            // panel18
+            // 
+            panel18.Location = new Point(223, 151);
+            panel18.Name = "panel18";
+            panel18.Size = new Size(104, 68);
+            panel18.TabIndex = 1;
+            // 
+            // panel19
+            // 
+            panel19.Location = new Point(333, 151);
+            panel19.Name = "panel19";
+            panel19.Size = new Size(104, 68);
+            panel19.TabIndex = 1;
+            // 
+            // panel20
+            // 
+            panel20.Location = new Point(553, 151);
+            panel20.Name = "panel20";
+            panel20.Size = new Size(104, 68);
+            panel20.TabIndex = 1;
+            // 
+            // panel21
+            // 
+            panel21.Location = new Point(443, 151);
+            panel21.Name = "panel21";
+            panel21.Size = new Size(104, 68);
+            panel21.TabIndex = 1;
+            // 
+            // panel22
+            // 
+            panel22.Location = new Point(663, 151);
+            panel22.Name = "panel22";
+            panel22.Size = new Size(104, 68);
+            panel22.TabIndex = 1;
+            // 
+            // EmployeeScheduling
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(812, 557);
+            Controls.Add(lbSaturday);
+            Controls.Add(lbFriday);
+            Controls.Add(tbThursday);
+            Controls.Add(lbWednesday);
+            Controls.Add(lbTuesday);
+            Controls.Add(lbMonday);
+            Controls.Add(lbSunday);
+            Controls.Add(btnPrevious);
+            Controls.Add(btnNext);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "EmployeeScheduling";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "EmployeeScheduling";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            flowLayoutPanel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button btnNext;
+        private Button btnPrevious;
+        private Label lbSunday;
+        private Label lbMonday;
+        private Label lbTuesday;
+        private Label lbWednesday;
+        private Label tbThursday;
+        private Label lbFriday;
+        private Label lbSaturday;
+        private Panel panel2;
+        private Panel panel1;
+        private Panel panel4;
+        private Panel panel3;
+        private Panel panel6;
+        private Panel panel5;
+        private Panel panel7;
+        private Panel panel8;
+        private Panel panel9;
+        private Panel panel10;
+        private Panel panel11;
+        private Panel panel12;
+        private Panel panel13;
+        private Panel panel15;
+        private Panel panel14;
+        private Panel panel16;
+        private Panel panel17;
+        private Panel panel18;
+        private Panel panel19;
+        private Panel panel21;
+        private Panel panel20;
+        private Panel panel22;
+    }
+}
