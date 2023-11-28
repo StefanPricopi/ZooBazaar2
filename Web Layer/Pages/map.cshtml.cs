48a2a51a31bb24047f51f60b16937249e7e0f15a
522201@student.fontys.nl <m.partalov@student.fontys.nl>
2023-11-28 23:53:49 +0100

    Added client's map to Map page, aswell as made buttons to switch the map between EN & NL

--- Web Layer/Pages/map.cshtml.cs
+++ Web Layer/Pages/map.cshtml.cs
@@ -5,8 +5,22 @@ namespace Web_Layer.Pages
 {
     public class mapModel : PageModel
     {
+        public string Img { get; set; } = "/img/map.png";
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPostEN()
+        {
+            Img = "/img/mapEN.png";
+            return Page();
+        }
+
+        public IActionResult OnPostNL()
+        {
+            Img = "/img/map.png";
+            return Page();
         }
     }
 }
